586eaf800105638013774fd13f31cee1e36d6ebe
sun-yryr <taittide@gmail.com>
2021-10-05 00:30:29 +0900

    a

--- bin/cancel-codepipeline.sh
+++ bin/cancel-codepipeline.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-if [ ! $# -eq 1 ]; then
+if [ ! $# -eq 12 ]; then
     echo '呼び出し失敗'
     exit 1
 fi
 
 # 引数取得
 pipeline_name=$1
+execution_id=$2
 
-status=$(aws codepipeline get-pipeline-execution --pipeline-name "${pipeline_name}" --pipeline-execution-id "${env.codepipeline_execution_id}" \
+status=$(aws codepipeline get-pipeline-execution --pipeline-name "${pipeline_name}" --pipeline-execution-id "${execution_id}" \
             | jq -r '.pipelineExecution.status')
 
 if [ "$status" = "InProgress" ]; then
-    execution_id=$(aws codepipeline stop-pipeline-execution --pipeline-name "${pipeline_name}" --pipeline-execution-id "${env.codepipeline_execution_id}" \
+    execution_id=$(aws codepipeline stop-pipeline-execution --pipeline-name "${pipeline_name}" --pipeline-execution-id "${execution_id}" \
                 | jq -r '.pipelineExecutionId')
     echo "停止しました：${execution_id}"
 fi
